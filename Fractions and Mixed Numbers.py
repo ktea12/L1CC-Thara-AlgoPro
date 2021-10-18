c689f2bc6210cf278c907c6a18ab5b8e6bdfd61c
Thara <91600181+ktea12@users.noreply.github.com>
2021-10-19 03:40:27 +0700

    Update Fractions and Mixed Numbers.py

--- Fractions and Mixed Numbers.py
+++ Fractions and Mixed Numbers.py
@@ -1,32 +1,35 @@
-#Forum Task: Fractions and Mixed Numbers
+#Forum Task Fractions
 
-num = eval(input("Enter a numerator that is greater than 0:"))
+num = eval(input("Enter a numerator that is greater than 0: "))
 while num<=0:
-    num = eval(input("Please re-enter the numerator. Value must be greater than 0:"))
+    num = eval(input("Please re-enter the numerator. Value must be greater than 0: "))
 
-denom = eval(input("Enter a denominator that is greater than 0:"))
+denom = eval(input("Enter a denominator that is greater than 0: "))
 while denom<=0:
-    denom = eval(input("Please re-enter the denominator. Value must be greater than 0:"))
+    denom = eval(input("Please re-enter the denominator. Value must be greater than 0: "))
+
 
 import math
-GCD = int(math.gcd(num, denom))
+GCD = math.gcd(num, denom)
 
 if num < denom:
     print (f'{num} / {denom} is a proper fraction.')
     if GCD == 1:
         print ('This proper fraction cannot be reduced any further.')
     else:
-        print (f'The proper fraction can be reduced to:')
+       print (f'The proper fraction can be reduced to: {int(num/GCD)} / {int(denom/GCD)}')
 
 elif num >= denom:
     print (f'{num} / {denom} is a improper fraction.')
     if GCD == 1:
         print ('This proper fraction cannot be reduced any further.')
     else:
-        print (f'The proper fraction can be reduced to:')
-
-    mix = num%denom   
-    if mix != 0:
-        print ('The mixed number is {mix}')
+        print (f'The proper fraction can be reduced to: {int(num/GCD)} / {int(denom/GCD)}')
+    
+    
+    mix = num // denom
+    remain = (int(num/GCD))%(int(denom/GCD))   
+    if remain != 0:
+        print (f'The mixed number is {mix} and {remain} / {int(denom/GCD)}')
     else:
-        print ('The whole number is {num}')
+        print (f'The whole number is {int(num/GCD)}')
