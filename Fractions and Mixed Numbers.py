806cc4c9cc0953e0750cd8a394133b9df30c4ba8
Thara <91600181+ktea12@users.noreply.github.com>
2021-10-19 07:16:49 +0700

    Update Fractions and Mixed Numbers.py

--- Fractions and Mixed Numbers.py
+++ Fractions and Mixed Numbers.py
@@ -11,25 +11,27 @@ while denom<=0:
 
 import math
 GCD = math.gcd(num, denom)
+renum = int(num/GCD)
+redenom = int(denom/GCD)
 
 if num < denom:
     print (f'{num} / {denom} is a proper fraction.')
     if GCD == 1:
         print ('This proper fraction cannot be reduced any further.')
     else:
-       print (f'The proper fraction can be reduced to: {int(num/GCD)} / {int(denom/GCD)}')
+       print (f'The proper fraction can be reduced to: {renum} / {redenom}')
 
 elif num >= denom:
     print (f'{num} / {denom} is a improper fraction.')
     if GCD == 1:
         print ('This proper fraction cannot be reduced any further.')
     else:
-        print (f'The proper fraction can be reduced to: {int(num/GCD)} / {int(denom/GCD)}')
+        print (f'The proper fraction can be reduced to: {renum} / {redenom}')
     
     
     mix = num // denom
-    remain = (int(num/GCD))%(int(denom/GCD))   
+    remain = (renum)%(redenom)   
     if remain != 0:
-        print (f'The mixed number is {mix} and {remain} / {int(denom/GCD)}')
+        print (f'The mixed number is {mix} and {remain} / {redenom}')
     else:
-        print (f'The whole number is {int(num/GCD)}')
+        print (f'The whole number is {renum}')
